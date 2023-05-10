41494906a1e90a389cd741250e6340678af8506b
JJHaEun <yj05037@gmail.com>
2023-05-10 19:31:15 +0900

    리덕스 스토어에 데이터 저장하기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetServerSideProps, NextPage } from "next";
 import { memo } from "react";
 import { wrapper } from "../src/commons/store";
+import { todoAction } from "../src/commons/store/todo";
 import TodoList from "../src/components/todoList/TodoList";
 import { IProps } from "../src/components/todoList/TodoList.types";
 import { getToDoAPI } from "../src/lib/api/todo";
@@ -17,19 +18,22 @@ const Main: NextPage<IProps> = ({ todos }): JSX.Element => {
 
 export default memo(Main);
 
-export const getServerSideProps = wrapper.getServerSideProps(async (store) => {
-  console.log("스토어", store);
-  try {
-    // console.log(process.env.NEXT_PUBLIC_API_URL, "서버");
-    const { data } = await getToDoAPI();
-    // console.log("res:", data);
-    // const res = await axios.get(
-    //   "http://localhost:3000/api/todos"
-    // );
-    // console.log("res:", res.data); // 위쪽과 동일한코드
-    return { props: { todos: data } };
-  } catch (error) {
-    console.log("에러", error);
-    return { props: { todos: [] } };
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    console.log("스토어", store);
+    try {
+      // console.log(process.env.NEXT_PUBLIC_API_URL, "서버");
+      const { data } = await getToDoAPI();
+      // console.log("res:", data);
+      // const res = await axios.get(
+      //   "http://localhost:3000/api/todos"
+      // );
+      // console.log("res:", res.data); // 위쪽과 동일한코드
+      store.dispatch(todoAction.setTodo(data));
+      return { props: { todos: data } };
+    } catch (error) {
+      console.log("에러", error);
+      return { props: { todos: [] } };
+    }
   }
-});
+);
